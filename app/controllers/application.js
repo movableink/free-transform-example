b190edef164981ad54899f7178f266e7910ff611
Michael Nutt <michael@nuttnet.net>
2015-02-25 01:08:24 -0500

    generate a bunch of randomly placed blocks to make the example more fun

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,40 @@
 import Em from 'ember';
 
 var ApplicationController = Em.Controller.extend({
-  models: [
-    Em.Object.create({top: 50, left: 50, width: 300, height: 200}),
-    Em.Object.create({top: 80, left: 400, width: 300, height: 200}),
-    Em.Object.create({top: 280, left: 100, width: 150, height: 200})
-  ]
+
+  // Brute-force function to generate a list of non-overlapping models
+  models: function() {
+    var x, y, found;
+
+    var count = 50;
+    var size = 50;
+    var width = 800;
+    var height = 600;
+    var affordance = size + 10;
+
+    var modelList = [];
+    for(var i = 0; i < count; i++) {
+      found = false;
+      while(!found) {
+        x = Math.floor(Math.random() * (width - size));
+        y = Math.floor(Math.random() * (height - size));
+
+        found = true;
+        for(var j = 0, len = modelList.length; j < len; j++) {
+          var model = modelList[j];
+          if(model.get('top') > y - affordance &&
+             model.get('top') < y + affordance &&
+             model.get('left') > x - affordance &&
+             model.get('left') < x + affordance) {
+            found = false;
+            break;
+          }
+        }
+      }
+      modelList.push(Em.Object.create({top: y, left: x, width: size, height: size}));
+    }
+    return modelList;
+  }.property()
 });
 
 export default ApplicationController;
